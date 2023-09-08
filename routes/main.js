6c89d6edd1fecfa0305f7dc2d522759075e98985
Elias-hd <jairgrubert53@gmail.com>
2023-09-08 08:20:18 -0300

    First commit

---
+++ routes/main.js
@@ -0,0 +1,13 @@
+__path = process.cwd()
+
+var express = require('express');
+var router = express.Router();
+
+router.get('/', (req, res) => {
+    res.sendFile(__path + '/views/home.html')
+})
+router.get('/docs', (req, res) => {
+    res.sendFile(__path + '/views/docs.html')
+})
+
+module.exports = router
